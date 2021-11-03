69561be2d3969fff55435d19b64a19cd03179942
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-11-03 08:39:10 +0330

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ console.log(`Your app is listening a http://localhost/${port}`)
 //require
 const Discord = require("discord.js");
 const config = require("./Storage/config.json");
-client.prefix = (message.prefix);
-
 const bot = new Discord.Client({
     disableEveryone: true,
     autoReconnect: true,
@@ -32,14 +30,38 @@ const load = async () => {
 
 
 //bot status
+const discord = require('discord.js');
+const client = new Discord.Client();
+Discord.Constants.DefaultOptions.ws.properties.$browser = "Discord Android"
 client.on("ready", () => {
   function YousamPower() {
     let hungry = [`${prefix}play` , `${prefix}help` ]
     let Power = Math.floor(Math.random() * hungry.length);
-    client.user.setActivity(hungry[Power], {type: "PLAYING"});//can be LISTENING, WATCHING, PLAYING, STREAMING
-  }; setInterval(YousamPower, 7000)
-    client.user.setStatus("dnd")//can be invesible, online, idle, dnd
+    client.user.setActivity(hungry[Power], {type: "LISTENING"});//can be LISTENING, WATCHING, PLAYING, STREAMING
+  }; setInterval(YousamPower, 5000)
 });
+client.login(config.token);
+
+//invite command
+const clinet = new Discord.Client();
+const prefix = 'BOT-PREFIX';
+bot.on('message', message => {
+    let args = message.content.substring(prefix.length).split("invite")
+
+    if (message.content.startsWith(`${prefix}invite`)) {
+        let inviteEmbed = new discord.MessageEmbed()
+      inviteEmbed.setAuthor(`Requested by ${message.author.username}`, `${message.author.displayAvatarURL()}`)
+      inviteEmbed.setThumbnail(message.client.user.displayAvatarURL({ format: "png" }))
+      inviteEmbed.setTitle(`Ba Invite Bot Be Servert Azash Hemaiat Kon☺ ${client.user.username}`)
+      inviteEmbed.setDescription(`Montazer chi hasti🤨? Bodo mano be servert add kon🙂😘 \n\n [Invite Link](https://discord.com/api/oauth2/authorize?client_id=${message.client.user.id}&permissions=137775017040&scope=bot)`)
+      inviteEmbed.setURL(`https://discord.gg/4pUbjscCmA`)
+      inviteEmbed.setFooter("Created By Sobhan.SRZA#2153 :)", `https://cdn.discordapp.com/attachments/902034619791196221/905054458793312327/2GU.gif`)
+      inviteEmbed.setColor("RANDOM")
+            message.channel.send(inviteEmbed)
+    }
+})
 
 load();
 bot.login(config.token);
+
+
