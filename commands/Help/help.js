1f3b2bf5d7a9db79a3d5ac16d477ea89ef2df74f
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-11-03 08:41:12 +0330

    Update help.js

--- commands/Help/help.js
+++ commands/Help/help.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const color = JSON.parse(fs.readFileSync(`Storage/color.json`, `utf8`));
+const prefix = 'BOT-PREFIX';
 
 exports.run = async (bot, message, args, functions) => {
 
@@ -14,9 +15,14 @@ let embed = new Discord.MessageEmbed()
    .setAuthor(`Ticket Help ${message.client.user.username }`,`${message.client.user.displayAvatarURL({ format: "png" })}`)
    .setThumbnail('https://cdn.discordapp.com/attachments/902034619791196221/905043997280567346/e4b59a9ad0fafa675c7760e7f6671d0e.gif')
    .setColor(`RANDOM`)
-   .addField("Set Kaedane Channel Baraie Log", "Az In Command Baraye Set Kardan Log Dar Channel Khod Estefade Konid ```;setlogs <Tag Channele Morede Nazar>```", false)
-   .addField("Sakht Ticket", "Az Command Baraye Sakhtane Ticket Estefade Konid ```;ticket```", false)
-   .addField("Bastan Ticket", "Az In Command Baraye Delete Kardan Ya Bastane Har Ticket Anjam Bedid ```;close```", false)
+   .addField("Set Kaedane Channel Baraie Log", `Az In Command Baraye Set Kardan Log Dar Channel Khod Estefade Konid 
+   > ${prefix}setlogs <Tag Channele Morede Nazar>`, false)
+   .addField("Sakht Ticket", `Az Command Baraye Sakhtane Ticket Estefade Konid 
+   > ${prefix}ticket`, false)
+   .addField("Bastan Ticket", `Az In Command Baraye Delete Kardan Ya Bastane Har Ticket Anjam Bedid 
+   > ${prefix}close`, false)
+   .addField("Gereftan Linke Invite", `Ba In Command Mitavand Link Invite Bot Ro Begirid
+   > ${prefix}invite`, false)
       
 .setFooter(`Requested by ${message.author.username} | Created By Sobhan.SRZA#2153 :)`, `${message.author.displayAvatarURL()}`)
    return message.channel.send(embed);
