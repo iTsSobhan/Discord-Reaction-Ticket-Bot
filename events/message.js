6a36e3aede0e83c286126232e819d42b3ae704a3
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-11-03 08:40:03 +0330

    Update message.js

--- events/message.js
+++ events/message.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const functions = require("../functions/functions.js");
 
 module.exports = async (bot, message) => {
-    let prefix = "BOT-PREFIX"
+const prefix = 'BOT-PREFIX';
     const args = message.content.split(/ +/g);
     const command = args.shift().slice(prefix.length).toLowerCase();
     const cmd = bot.commands.get(command) || bot.aliases.get(command);
