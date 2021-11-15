96b8c8d0b2c817772c4cd4f01614437aa62a86e9
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-11-15 10:13:36 +0330

    Update help.js

--- commands/Help/help.js
+++ commands/Help/help.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const color = JSON.parse(fs.readFileSync(`Storage/color.json`, `utf8`));
-const prefix = ';';
+const prefix = 'add-prefix';
 
 exports.run = async (bot, message, args, functions) => {
 
