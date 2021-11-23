fbd8485108383800053bca6597f0f12d42b03e77
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-11-23 20:34:11 +0330

    Update ticket.js

--- commands/Utilities/ticket.js
+++ commands/Utilities/ticket.js
@@ -13,7 +13,7 @@ let embed = new Discord.MessageEmbed()
       .setThumbnail('https://cdn.discordapp.com/attachments/902034619791196221/905040476355330068/8b7193b2110a034a2fe037437afc80b3.gif')
 .addField('Baraye Sakhte Ticket Roye 🎟️ Bezanid','Ba Sakhte Shodane Channele Gadid Bama Mitavanid Sohbat Konid📩')
 .setURL('https://discord.gg/xgcZNSjanP')
-.setFooter(`Requested by ${message.author.username} | Created By Sobhan.SRZA#2153 :)`, `${message.author.displayAvatarURL()}`)
+.setFooter(`Requested by ${message.author.username} | Created By Mr.SIN RE#1528 :)`, `${message.author.displayAvatarURL()}`)
 message.channel.send(embed).then(m => {
   m.react('🎟️');
 });
